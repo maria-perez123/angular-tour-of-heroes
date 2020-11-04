a386e3aee26615e6f8faff01341beb587325602f
María Pérez <maria_erez@hotmail.es>
2020-11-04 15:25:32 -0500

    addding new tests

--- e2e/src/integration/app.spec.ts
+++ e2e/src/integration/app.spec.ts
@@ -2,7 +2,14 @@ import { getGreeting } from '../support/app.po';
 
 describe('angular-tour-of-heroes', () => {
 /*
-  it('testing routes', () => {
+it('should have the title 'Tour Of Heroes', ()=>{
+  cy.visit('/')
+  cy.contains('Tour of Heroes', {mathCase:false})
+})
+
+
+
+it('testing routes', () => {
     cy.visit('/') //testing visit to home/dashboard
     cy.url().should('include', '/dashboard')
     cy.get('[data-cy="nav-heroes"]').click()  //testing heroes button
@@ -55,13 +62,43 @@ describe('angular-tour-of-heroes', () => {
       const id=currentURL[4]
       cy.get('[data-cy="div-id"]').invoke('text').should('include', id )
     })
-  })*/
+  })*//*
   it('testing name in input is equal to title in uppercase', ()=>{
-    cy.visit('/detail/11')
+    cy.visit('/heroes')
+    cy.get(':nth-child(1) > [data-cy=list-of-heroes]').click()
+    cy.get('[data-cy="input-name"]').clear().type('Tornado')
     cy.get('[data-cy="input-name"]').invoke('attr', 'ng-reflect-model').then(text=>{
       const texto=text.toUpperCase()
       console.log(text)
       cy.get('[data-cy="titleherodetail"]').invoke('text').should('include', texto)
-    }) 
+    })
+    cy.get('[ data-cy="save-button"]').click()
+    cy.get(':nth-child(1) > [data-cy=list-of-heroes]').invoke('text').should('contain', 'Tornado')
+  })*/
+/*
+  it('testing name in input is equal to title in uppercase', ()=>{
+    cy.visit('/heroes')
+    cy.get(':nth-child(1) > [data-cy=list-of-heroes]').click()
+    cy.get('[data-cy="input-name"]').clear().type('WWWWWWWWWW')
+    cy.get('[ data-cy="goback-button"]').click()
+    cy.get(':nth-child(1) > [data-cy=list-of-heroes]').invoke('text').should('not.contain', 'WWWWWWWWWW')
+  })*/
+/*
+  it('testing name in input is equal to title in uppercase', ()=>{
+    cy.visit('/heroes')
+    cy.get('[data-cy="addhero-input"]').clear().type('name')
+    cy.get('[data-cy="add-button"]').click()
+    cy.wait(500)
+    cy.get(':last-child() > [data-cy=list-of-heroes]').invoke('text').should('contain', 'name')
+    cy.get('[data-cy=list-of-heroes]').should('have.length', 11)
+  })*/
+  it('testing name in input is equal to title in uppercase', ()=>{
+    cy.visit('/heroes')
+    cy.get(':last-child() > [data-cy=list-of-heroes]').invoke('text').then(text=>{
+      cy.get(':last-child()>[data-cy="delete-button"]').click()
+      cy.wait(500)
+      cy.get(':last-child() > [data-cy=list-of-heroes]').invoke('text').should('not.contain', text)
+    })
+    cy.get('[data-cy=list-of-heroes]').should('have.length', 9)
   })
 });
