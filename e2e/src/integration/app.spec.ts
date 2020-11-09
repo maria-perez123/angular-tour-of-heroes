ef11f2d60ea7311dd900a4be30d97235e4208bb3
María Pérez <maria_erez@hotmail.es>
2020-11-08 22:30:29 -0500

    actual tests ordered

--- e2e/src/integration/app.spec.ts
+++ e2e/src/integration/app.spec.ts
@@ -8,22 +8,23 @@ import {
   messageNotfoundHero} from '../support/app.po'
 
 
-describe('angular-tour-of-heroes', () => {
-/*
+describe('search filter and message division tests', () => {
+  beforeEach(()=>{
+    cy.visit('/')
+  })
+
   it('it test the titles and messages of fetched heroes appears', ()=>{
     cy.visit('/')
-    cy.contains('TOUR OF HEROES', {matchCase:false})
+    cy.contains('TOP HEROES', {matchCase:false})
     messageFetchHeroes()
     cy.visit('/heroes')
     cy.contains('MY HEROES', {matchCase:false})
     messageFetchHeroes()
   })
-*//*
+
   it('it test the  search filter', () => {
-    cy.visit('/')
     const term='m'
     cy.get('input').type(term)
-
     cy.get('[data-y="search-hero"]').each(($li) => {
       cy.wrap($li).invoke('text').should(text => {
         expect(text.toLowerCase()).to.include(term);
@@ -36,26 +37,28 @@ describe('angular-tour-of-heroes', () => {
     })
     cy.url().should('include', '/detail/')
   });
-*/
-/*
+
   it('it test the search filter doesnt show anything if the search word doesnt appear', () => {
-    cy.visit('/')
     const term='WWWWWW'
     cy.get('input').type(term)
     cy.get('[data-y="search-hero"]').should('have.length', 0)
     messageNotfoundHero(term)
   });
-*/
-/*
+
   it('it test the messages can be deleted with the "clear"button ', ()=>{
-    cy.visit('/')
     cy.get('[data-cy=messages-div]').should('exist');
     cy.get('[data-cy="clear-button"]').click()
     cy.get('[data-cy=messages-div]').should('not.exist');
   })
-*/
-/*
-  it('it test hero id is contain in the detail url and messega of fetch appear', ()=>{
+
+})
+
+describe('Hero related tests', () => {
+  beforeEach(()=>{
+    cy.visit('/heroes')
+  })
+
+  it('it test the hero id is contain in the detail url and messages of fetch appear', ()=>{
     cy.visit('/detail/11')
     cy.url().then(url=>{
       const id=url.substr(url.lastIndexOf('/')+1)
@@ -63,10 +66,8 @@ describe('angular-tour-of-heroes', () => {
       messageFetchHero(id)
     })
   })
-*/
-/*
+
   it('it test the hero name can be updated, and the title in hero datail contains the hero name in uppercase', ()=>{
-    cy.visit('/heroes')
     cy.get(':nth-child(1) > [data-cy=list-of-heroes]').click()
     cy.get('[data-cy="input-name"]').clear().type('Tornado')
     cy.get('[data-cy="input-name"]').invoke('attr', 'ng-reflect-model').then(text=>{
@@ -80,20 +81,16 @@ describe('angular-tour-of-heroes', () => {
     })
     cy.get(':nth-child(1) > [data-cy=list-of-heroes]').invoke('text').should('contain', 'Tornado')
   })
-*/
-/*
+
   it('it test the hero name shouldnt update when is pressed the goback button', ()=>{
-    cy.visit('/heroes')
     cy.get(':nth-child(1) > [data-cy=list-of-heroes]').click()
     cy.get('[data-cy="input-name"]').clear().type('WWWWWWWWWW')
     cy.get('[ data-cy="goback-button"]').click()
     cy.get(':nth-child(1) > [data-cy=list-of-heroes]').invoke('text').should('not.contain', 'WWWWWWWWWW')
   })
-*/
 
   it('it test a hero can be added and the number of elements in the heroes list is increased by one', ()=>{
     const newheroname='newheroname'
-    cy.visit('/heroes')
     cy.get('[data-cy=list-of-heroes]').its('length').then(length=>{
       cy.get('[data-cy="addhero-input"]').clear().type(newheroname)
       cy.get('[data-cy="add-button"]').click()
@@ -106,11 +103,8 @@ describe('angular-tour-of-heroes', () => {
     })
   })
 
-/*
-  it('it test the a hero can be deleted and the number o elements is decrease by one', ()=>{
-    cy.visit('/heroes')
+  it('it test a hero can be deleted and the number of elements is decrease by one', ()=>{
     cy.get('[data-cy=list-of-heroes]').its('length').then(length=>{
-
       cy.get(':last-child() > [data-cy=list-of-heroes]').invoke('text').then(text=>{
         const id=text.substr(0, text.indexOf(' '))
         cy.get(':last-child()>[data-cy="delete-button"]').click()
@@ -118,9 +112,7 @@ describe('angular-tour-of-heroes', () => {
         messageDeleteHero(id)
         cy.get(':last-child() > [data-cy=list-of-heroes]').invoke('text').should('not.contain', text)
       })
-
     cy.get('[data-cy=list-of-heroes]').should('have.length', length-1)
     })
   })
-*/
 });
