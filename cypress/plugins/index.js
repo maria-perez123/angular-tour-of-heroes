aab68ade69429cf3b977f9553e350e7d537ca0aa
María Pérez <maria_erez@hotmail.es>
2020-11-22 21:23:31 -0500

    adding coverage

---
+++ cypress/plugins/index.js
@@ -0,0 +1,7 @@
+const cypressTypeScriptPreprocessor = require("./cy-ts-preprocessor");
+
+module.exports = (on, config) => {
+  on("file:preprocessor", cypressTypeScriptPreprocessor);
+  require('@cypress/code-coverage/task')(on, config);
+  return config;
+};
